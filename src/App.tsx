3524a315bc481ad0c33ce61bf5c8213197afd278
Ben van de Sande <ben@pomodoro.nl>
2021-05-28 15:40:09 +0200

    remove pre-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import './App.css';
+import "./App.css";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import logo from './logo.svg';
+import logo from "./logo.svg";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -21,7 +21,10 @@ function App() {
             🪂 Click me : {count}
           </button>
 
-          <p> Don&apos;t forgot to install Eslint and Prettier in Your Vscode.</p>
+          <p>
+            {" "}
+            Don&apos;t forgot to install Eslint and Prettier in Your Vscode.
+          </p>
 
           <p>
             Mess up the code in <code>App.tsx </code> and save the file.
@@ -31,15 +34,17 @@ function App() {
               className="App-link"
               href="https://reactjs.org"
               target="_blank"
-              rel="noopener noreferrer">
+              rel="noopener noreferrer"
+            >
               Learn React
             </a>
-            {' | '}
+            {" | "}
             <a
               className="App-link"
               href="https://vitejs.dev/guide/features.html"
               target="_blank"
-              rel="noopener noreferrer">
+              rel="noopener noreferrer"
+            >
               Vite Docs
             </a>
           </p>
